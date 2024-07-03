316b233fb95f9cf5864251a347768efad9da1130
julia-kocik <julia.kocik@gmail.com>
2024-07-03 17:00:21 +0200

    Add changes

--- src/About/About.tsx
+++ src/About/About.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 import "./About.scss";
-
+import { MdWorkOutline } from "react-icons/md";
+import { FiMapPin } from "react-icons/fi";
+import { LiaHandshake } from "react-icons/lia";
 const About = () => {
   return (
     <div id="about" className="about_container">
-      <div className="about_content"></div>
+      <div className="about_content">
+        <p>
+          Profesjonalni specjaliści z wieloletnim doświadczeniem
+          <span>
+            <MdWorkOutline />
+          </span>
+        </p>
+        <p>
+          Działamy w województwie mazowieckim, śląskim i opolskim
+          <span>
+            <FiMapPin />
+          </span>
+        </p>
+        <p>
+          Kompleksowe rozwiązania dopasowane do potrzeb klientów
+          <span>
+            <LiaHandshake />
+          </span>
+        </p>
+      </div>
     </div>
   );
 };
--- src/Offer/Offer.tsx
+++ src/Offer/Offer.tsx
@@ -4,7 +4,20 @@ import "./Offer.scss";
 const Offer = () => {
   return (
     <div id="offer" className="offer_container">
-      <div className="offer_content"></div>
+      <div className="offer_content">
+        <ul>
+          <li>
+            Konserwacja, przeglądy i naprawy urządzeń i instalacji
+            elektrycznych, grzewczych, gazowych, wentylacji, chłodu,
+            klimatyzacji i kontroli dostępu
+          </li>
+          <li>Obsługa BMS.</li>
+          <li>Przeglądy i naprawy systemu CCTV.</li>
+          <li>Kontrola systemów ppoż. oraz SAP.</li>
+          <li>Serwis Klimatyzacji.</li>
+          <li>Prowadzenie dokumentacji UDT.</li>
+        </ul>
+      </div>
     </div>
   );
 };
